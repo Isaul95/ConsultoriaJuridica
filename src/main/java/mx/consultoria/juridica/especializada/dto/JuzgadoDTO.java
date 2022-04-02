506b3ee4bcf8a1e6d81ae6e014e88e70c049e20d
eliza <eliza@LAPTOP-EVL2SSF6>
2022-04-02 14:00:22 -0600

    cambios de eli

--- src/main/java/mx/consultoria/juridica/especializada/dto/JuzgadoDTO.java
+++ src/main/java/mx/consultoria/juridica/especializada/dto/JuzgadoDTO.java
@@ -2,20 +2,37 @@ package mx.consultoria.juridica.especializada.dto;
 
 public class JuzgadoDTO {
 
-	private int idJuzgado;
-	private String nombre;
+	private Long idJuzgado;
 	
-	public int getIdJuzgado() {
+	private String juzgado;
+	
+	private String ramo;
+
+	public Long getIdJuzgado() {
 		return idJuzgado;
 	}
-	public void setIdJuzgado(int idJuzgado) {
+
+	public void setIdJuzgado(Long idJuzgado) {
 		this.idJuzgado = idJuzgado;
 	}
-	public String getNombre() {
-		return nombre;
+
+	public String getJuzgado() {
+		return juzgado;
 	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
+
+	public void setJuzgado(String juzgado) {
+		this.juzgado = juzgado;
 	}
+
+	public String getRamo() {
+		return ramo;
+	}
+
+	public void setRamo(String ramo) {
+		this.ramo = ramo;
+	}
+	
+	
+	
 	
 }
